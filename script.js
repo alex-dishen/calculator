aeeebe6468a277c042979d7cc88f776a3a9a2d7b
Alex <didyshen.oleksandr@gmail.com>
2022-07-29 18:15:14 +0300

    Fix issues when equal is pressed

--- script.js
+++ script.js
@@ -3,53 +3,68 @@ const result = document.querySelector('.result');
 const typed = document.querySelector('.typed');
 const clean = document.querySelector('.clean');
 
-let typingNum = '';
-let typedNum = '';
-let operator = '';
+let oldNum = '';
+let currNum = '';
+let operation = '';
 let clicks = 0;
+let equalClicks = 0;
 
 window.onload = () => {
     result.textContent = '0';
 }
 
-function populateDisplay(e) {
-    const className = e.target.className;
-    const value = e.target.value;
-
-    if(value >= 0) {
-        for(let i = 0; i < 1; i++) {
-            typingNum += value
-        }
-        result.textContent = typingNum
-    }
-
-    if (className === 'operator active') {
-        if (clicks > 0) {
-            if (typingNum) {
-                typedNum = operate(typedNum, operation, typingNum);
-            }
-            typed.textContent = `${typedNum} ${operation}`;
-        } else {
-            typedNum = typingNum;
-        }
-
-        operation = value;
-        typed.textContent = `${typedNum} ${operation}`;
-        result.textContent = typedNum;
-        typingNum = '';
-        ++clicks;
-    }
-// 12 + 7 - 5 * 3 = should yield 42
-    calculateOnEqual(e);
-}
-
 function calculateOnEqual(e) {
     const className = e.target.className;
     if (className === 'equal active') {
-        typingNum = operate(typedNum, operation, typingNum);
-        result.textContent = typingNum
-        console.log(`On equal: ${typingNum}`);
+        if (oldNum && currNum) {
+            typed.textContent = `${currNum} ${operation} ${oldNum} =`;
+            oldNum = operate(currNum, operation, oldNum);
+            currNum = oldNum;
+            result.textContent = oldNum;
+        }
+        oldNum = '';
+        ++equalClicks;
     }
+}
+
+function calculateOnOperator(e) {
+    const className = e.target.className;
+    const value = e.target.value;
+
+    if (className === 'operator active') {
+        if (clicks > 0) {
+            if (oldNum) {
+                currNum = operate(currNum, operation, oldNum);
+            }
+            typed.textContent = `${currNum} ${operation}`;
+        } else {
+            currNum = oldNum;
+        }
+
+        operation = value;
+        typed.textContent = `${currNum} ${operation}`;
+        result.textContent = currNum;
+        oldNum = '';
+        ++clicks;
+    }
+}
+
+function populateDisplay(e) {
+    const value = e.target.value;
+
+    if (currNum === '') {
+        typed.textContent = '';
+    }
+    
+    if(value >= 0) {
+        for(let i = 0; i < 1; i++) {
+            oldNum += value;
+        }
+        result.textContent = oldNum;
+    }
+
+    calculateOnOperator(e);
+    calculateOnEqual(e);
 }
 
 function addActiveClass(e) {
@@ -57,17 +72,8 @@ function addActiveClass(e) {
 }
 
 function removeActiveClass(e) {
-    e.target.classList.remove('active')
+    e.target.classList.remove('active');
 }
-
-buttons.forEach(button => {
-    button.addEventListener('mousedown', (e) => {
-        addActiveClass(e);
-        populateDisplay(e);
-    });
-    button.addEventListener('mouseup', removeActiveClass);
-    button.addEventListener('mouseleave', removeActiveClass);
-})
 
 function add(a, b) {
     return +a + +b
@@ -97,11 +103,21 @@ function operate (a, operator, b) {
     }
 }
 
+buttons.forEach(button => {
+    button.addEventListener('mousedown', (e) => {
+        addActiveClass(e);
+        populateDisplay(e);
+    });
+    button.addEventListener('mouseup', removeActiveClass);
+    button.addEventListener('mouseleave', removeActiveClass);
+})
+
 clean.addEventListener('click', () => {
-    typedNum = '';
-    typingNum = '';
+    currNum = '';
+    oldNum = '';
     operation = '';
     typed.textContent = '';
     result.textContent = '0';
     clicks = 0;
+    equalClicks = 0;
 });
