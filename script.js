d5f4f8ed58802821af78d17f3340b29f680837a3
Alex <didyshen.oleksandr@gmail.com>
2022-07-29 18:39:49 +0300

    Remove math from separate functions

--- script.js
+++ script.js
@@ -75,32 +75,11 @@ function removeActiveClass(e) {
     e.target.classList.remove('active');
 }
 
-function add(a, b) {
-    return +a + +b
-}
-
-function subtract(a, b) {
-    return a - b
-}
-
-function multiply (a, b) {
-    return a * b
-}
-
-function divide (a, b) {
-    return a / b
-}
-
-function operate (a, operator, b) {
-    if(operator === '+') {
-        return add(a, b)
-    } else if (operator === '-') {
-        return subtract(a, b)
-    } else if (operator === '*') {
-        return multiply (a, b)
-    } else if (operator === '/') {
-        return divide (a, b)
-    }
+function operate (firstNumber, operator, secondNumber) {
+    if(operator === '+') return +firstNumber + +secondNumber;
+    if (operator === '-') return firstNumber - secondNumber;
+    if (operator === '*') return firstNumber * secondNumber;
+    if (operator === '/') return firstNumber / secondNumber;
 }
 
 buttons.forEach(button => {
