bb148be1c4612eb09a4b6261918780e01a22c3b6
Alex <didyshen.oleksandr@gmail.com>
2022-07-29 12:03:45 +0300

    Create changing display

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+const typed = document.querySelector('.typed');
+let type = '';
+let displayText = '';
+
+window.onload = () => {
+    result.textContent = '0';
+}
+
+function populateDisplay(e) {
+    if(e.target.value >= 0) {
+        for(let i = 0; i < 1; i++) {
+            type += e.target.value
+        }
+    }
+    result.textContent = type
+
+    if (e.target.className === 'operator active') {
+        displayText = `${type} ${e.target.value}`
+        typed.textContent = displayText
+        type = ''
+    }
+}
+
+function addActiveClass(e) {
+    e.target.classList.add('active');
+}
+
+function removeActiveClass(e) {
+    e.target.classList.remove('active')
+}
 
 buttons.forEach(button => {
-    button.addEventListener('mousedown', addActiveClass);
+    button.addEventListener('mousedown', (e) => {
+        addActiveClass(e);
+        populateDisplay(e)
+    });
     button.addEventListener('mouseup', removeActiveClass);
     button.addEventListener('mouseleave', removeActiveClass);
 })
