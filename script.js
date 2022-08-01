efb5e290ffcacee0496dfe59d09d8017a92c81dd
Alex <didyshen.oleksandr@gmail.com>
2022-08-01 20:01:53 +0300

    Add evaluate funtcion

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const numberButtons = document.querySelectorAll('[data-number]');
 const currentOperationScreen = document.querySelector('.result');
-const clean = document.querySelector('.clean');
+const lastOperationScreen = document.querySelector('.typed');
+const deleteBtn = document.querySelector('.backspace');
+
+let firstNumber = '';
+let lastNumber = '';
+let operation = '';
 
 function operate (firstNumber, operator, secondNumber) {
     firstNumber = Number(firstNumber);
@@ -16,6 +21,12 @@ function setNumber(number) {
     currentOperationScreen.textContent += number
 }
 
+function setOperator(operator) {
+    firstNumber = currentOperationScreen.textContent;
+    operation = operator;
+    lastOperationScreen.textContent = `${firstNumber} ${operation}`;
+}
+
 function colorButton(e) {
     const value = document.querySelector(`button[value='${e.key}']`);
     value.classList.add('active');
@@ -27,14 +38,24 @@ function deleteNumber() {
     currentOperationScreen.textContent.slice(0, -1);
 }
 
+function evaluate() {
+    currentOperationScreen.textContent = 
+    operate(firstNumber, operation, lastNumber);
+    lastOperationScreen.textContent = `${firstNumber} ${operation} ${lastNumber} =`
+    // currentOperation = null
+}
+
 function handleKeyboardINput(e) {
     if (e.key >= 0) setNumber(e.key);
-    if (e.key === 'Backspace') deleteNumber()
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') 
+        setOperator(e.key);
+    if (e.key === '=' || e.key === 'Enter') evaluate();
+    if (e.key === 'Backspace') deleteNumber();
     colorButton(e);
 }
 
 window.addEventListener('keydown', handleKeyboardINput);
-
 numberButtons.forEach(number => {
     number.addEventListener('click', () => {setNumber(number.value)})
 });
+deleteBtn.addEventListener('click', deleteNumber);
