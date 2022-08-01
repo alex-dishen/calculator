b9a02d6f4b4e321fee7ca43a63a7a21b95d0d8e0
Alex <didyshen.oleksandr@gmail.com>
2022-08-01 12:34:31 +0300

    Add appeatence of buttond on the ckick

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll('button');
+const numberButtons = document.querySelectorAll('[data-number]');
 const result = document.querySelector('.result');
 const typed = document.querySelector('.typed');
 const clean = document.querySelector('.clean');
@@ -9,132 +10,132 @@ let currNum = '';
 let operation = '';
 let clicks = 0;
 
-function calculateOnEqual(e) {
-    let className = e.target.className;
+// function calculateOnEqual(e) {
+//     let className = e.target.className;
 
-    //KEYBOARD CONNECTION
-    if(e.key) {
-        const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
-        className = key.className
-    }
+//     //KEYBOARD CONNECTION
+//     if(e.key) {
+//         const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+//         className = key.className
+//     }
 
-    if (className === 'equal active') {
-        if (oldNum && currNum) {
-            typed.textContent = `${currNum} ${operation} ${oldNum} =`;
-            currNum = operate(currNum, operation, oldNum);
-            result.textContent = currNum;
-        }
+//     if (className === 'equal active') {
+//         if (oldNum && currNum) {
+//             typed.textContent = `${currNum} ${operation} ${oldNum} =`;
+//             currNum = operate(currNum, operation, oldNum);
+//             result.textContent = currNum;
+//         }
 
-        oldNum = ''
-    }
-}
+//         oldNum = ''
+//     }
+// }
 
-function calculateOnOperator(e) {
-    let className = e.target.className;
-    let value = e.target.value;
+// function calculateOnOperator(e) {
+//     let className = e.target.className;
+//     let value = e.target.value;
 
-    //KEYBOARD CONNECTION
-    if(e.key) {
-        const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
-        value = key.value;
-        className = key.className
-    }
+//     //KEYBOARD CONNECTION
+//     if(e.key) {
+//         const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+//         value = key.value;
+//         className = key.className
+//     }
 
-    if (className === 'operator active') {
-        if (clicks > 0) {
-            if (oldNum || currNum) {
-                currNum = operate(currNum, operation, oldNum);
-                typed.textContent = `${currNum} ${operation}`;
-            }
-        } else {
-            currNum = oldNum;
-        }
+//     if (className === 'operator active') {
+//         if (clicks > 0) {
+//             if (oldNum || currNum) {
+//                 currNum = operate(currNum, operation, oldNum);
+//                 typed.textContent = `${currNum} ${operation}`;
+//             }
+//         } else {
+//             currNum = oldNum;
+//         }
 
-        operation = value;
-        if (currNum) {
-            typed.textContent = `${currNum} ${operation}`;
-            result.textContent = currNum;
-        }
+//         operation = value;
+//         if (currNum) {
+//             typed.textContent = `${currNum} ${operation}`;
+//             result.textContent = currNum;
+//         }
 
-        oldNum = '';
-        ++clicks;
-    }
-}
+//         oldNum = '';
+//         ++clicks;
+//     }
+// }
 
-function populateDisplay(e) {
-    let value = e.target.value;
+// function populateDisplay(e) {
+//     let value = e.target.value;
 
-    //KEYBOARD CONNECTION
-    if(e.key) {
-        const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
-        value = key.value;
-    }
+//     //KEYBOARD CONNECTION
+//     if(e.key) {
+//         const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+//         value = key.value;
+//     }
 
-    if (currNum === '') {
-        typed.textContent = '';
-    }
+//     if (currNum === '') {
+//         typed.textContent = '';
+//     }
     
-    if(value >= 0 || value === '.') {
-        oldNum += value;
-        result.textContent = oldNum;
-    }
+//     if(value >= 0 || value === '.') {
+//         oldNum += value;
+//         result.textContent = oldNum;
+//     }
 
-    calculateOnOperator(e);
-    calculateOnEqual(e);
-}
+//     calculateOnOperator(e);
+//     calculateOnEqual(e);
+// }
 
-function addActiveClass(e) {
-    e.target.classList.add('active');
-}
+// function addActiveClass(e) {
+//     e.target.classList.add('active');
+// }
 
-function removeActiveClass(e) {
-    e.target.classList.remove('active');
-}
+// function removeActiveClass(e) {
+//     e.target.classList.remove('active');
+// }
 
 
 //KEYBOARD CONNECTION
-function addActiveToKeyboard(e) {
-    const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
-    key.classList.add('active');
-}
+// function addActiveToKeyboard(e) {
+//     const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+//     key.classList.add('active');
+// }
 
-function removeActiveFromKeyboard(e) {
-    const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
-    key.classList.remove('active');
-}
+// function removeActiveFromKeyboard(e) {
+//     const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+//     key.classList.remove('active');
+// }
 
-function removeLastElement(string) {
-    oldNum = string.slice(0, string.length - 1);
-    result.textContent = oldNum;
-}
+// function removeLastElement(string) {
+//     oldNum = string.slice(0, string.length - 1);
+//     result.textContent = oldNum;
+// }
 
-function cleanDisplay() {
-    currNum = '';
-    oldNum = '';
-    operation = '';
-    typed.textContent = '';
-    result.textContent = '';
-    clicks = 0;
-}
+// function cleanDisplay() {
+//     currNum = '';
+//     oldNum = '';
+//     operation = '';
+//     typed.textContent = '';
+//     result.textContent = '';
+//     clicks = 0;
+// }
 
-function operate (firstNumber, operator, secondNumber) {
-    firstNumber = Number(firstNumber);
-    secondNumber = Number(secondNumber);
+// function operate (firstNumber, operator, secondNumber) {
+//     firstNumber = Number(firstNumber);
+//     secondNumber = Number(secondNumber);
 
-    if (operator === '+') return firstNumber + secondNumber;
-    if (operator === '-') return firstNumber - secondNumber;
-    if (operator === '*') return firstNumber * secondNumber;
-    if (operator === '/') return firstNumber / secondNumber;
-}
+//     if (operator === '+') return firstNumber + secondNumber;
+//     if (operator === '-') return firstNumber - secondNumber;
+//     if (operator === '*') return firstNumber * secondNumber;
+//     if (operator === '/') return firstNumber / secondNumber;
+// }
 
-buttons.forEach(button => {
-    button.addEventListener('mousedown', (e) => {
-        addActiveClass(e);
-        populateDisplay(e);
-    });
-    button.addEventListener('mouseup', removeActiveClass);
-    button.addEventListener('mouseleave', removeActiveClass);
-})
+// buttons.forEach(button => {
+//     button.addEventListener('mousedown', (e) => {
+//         addActiveClass(e);
+//         populateDisplay(e);
+//     });
+//     button.addEventListener('mouseup', removeActiveClass);
+//     button.addEventListener('mouseleave', removeActiveClass);
+// })
 
 function setNumber(number) {
     result.textContent += number
@@ -145,11 +146,14 @@ function handleKeyboardINput(e) {
     if (e.key === '') {}
 }
 
-clean.addEventListener('click', cleanDisplay);
+// clean.addEventListener('click', cleanDisplay);
 
-backspace.addEventListener('click', () => {removeLastElement(oldNum)});
+// backspace.addEventListener('click', () => {removeLastElement(oldNum)});
 
-//KEYBOARD CONNECTION
 window.addEventListener('keydown', handleKeyboardINput);
+
+numberButtons.forEach(number => {
+    number.addEventListener('click', () => setNumber(number.textContent))
+});
 
 // window.addEventListener('keyup', removeActiveFromKeyboard);
